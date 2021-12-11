2527c3b2387c9ee969386b37552af61021109736
Laguna1 <lagunazoomarket@gmail.com>
2021-12-12 00:37:09 +0200

    Add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-end
+
+  validates :username, presence: true, 
+                       uniqueness: {case_sensitive: false}
+  validates :email, presence: true, 
+                    uniqueness: {case_sensitive: false},
+                    format:{with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
+  validates :mobile_no, uniqueness: true, numericality: { only_integer: true }
+end 
