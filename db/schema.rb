d8f58716a949728358e9897b17bf3c1ace26b6d2
Laguna1 <lagunazoomarket@gmail.com>
2021-12-12 23:22:18 +0200

    Add article_categories join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_204201) do
+ActiveRecord::Schema.define(version: 2021_12_12_212100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_catgories", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_article_catgories_on_article_id"
+    t.index ["category_id"], name: "index_article_catgories_on_category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -43,4 +52,6 @@ ActiveRecord::Schema.define(version: 2021_12_12_204201) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "article_catgories", "articles"
+  add_foreign_key "article_catgories", "categories"
 end
