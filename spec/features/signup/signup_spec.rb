1736ce430eb5374250ed26d92cccafde5ee937fd
Laguna1 <lagunazoomarket@gmail.com>
2021-12-13 22:24:20 +0200

    Attempt to add sign_up feature test

---
+++ spec/features/signup/signup_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Signup Page', type: :feature do
+  describe 'User signs up', type: :feature do
+    let!(:fullname) { 'fullname' }
+    let!(:mobile_no) { '380_501_111_112' }
+    let!(:email) { 'user@email.com' }
+    let!(:password) { 'password' }
+
+    it "creates new user" do
+        visit 'new_user_registration_path'
+        within('form', visible: false) do
+          fill_in 'user_fullname', with: fullname
+          fill_in 'user[mobile_no]', with: mobile_no
+          fill_in 'user[email]', with: email
+          fill_in 'user[password]', with: password
+          fill_in 'user[password_confirmation]', with: password
+        end
+        click_button 'Submit'
+        expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+    # before do
+    #   visit new_user_registration_path
+    # end
+
+    # scenario 'with valid credentials' do
+    #   within('form') do
+    #     fill_in 'user[fullname]', with: fullname
+    #     fill_in 'user[mobile_no]', with: mobile_no
+    #     fill_in 'user[email]', with: email
+    #     fill_in 'user[password]', with: password
+    #     fill_in 'user[password_confirmation]', with: password
+    #   end
+    #   click_button 'Submit'
+    #   expect(page).to have_content('Welcome! You have signed up successfully.')
+    # end
+
+    # scenario 'invalid when email already exists' do
+    #   new_user = create(:user)
+    #   fill_in 'user_fullname', with: new_user.fullname
+    #   fill_in 'email', with: new_user.email
+    #   fill_in 'mobile_no', with: new_user.mobile_no
+    #   fill_in 'password', with: new_user.password
+    #   fill_in 'password_confirmation', with: new_user.password
+    #   click_button 'Submit'
+    #   expect(page).to have_text 'Email has already been taken'
+    # end
+  end
+end
